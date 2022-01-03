3cc6a799d2db5d94cafd9de51e7c6e18b9f46922
leona <leona08@naver.com>
2022-01-03 21:54:07 +0900

    init commit

---
+++ App.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, View, Text, Button, Image, Alert} from 'react-native';
+
+const App = () => {
+  return <SafeAreaView style={{flex: 1}}></SafeAreaView>;
+};
+
+export default App;
