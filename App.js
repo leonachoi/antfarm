b406d6e63ddad66f39e7af24e4cd91feeca465be
최명진 <mj@dasomx.com>
2022-01-03 22:13:36 +0900

    install rnge and sample

--- App.js
+++ App.js
@@ -6,11 +6,38 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import {SafeAreaView, View, Text, Button, Image, Alert} from 'react-native';
+import React, {PureComponent} from 'react';
+import {AppRegistry, StyleSheet, StatusBar} from 'react-native';
+import {GameEngine} from 'react-native-game-engine';
+import Finger from './src/renderers/Finger';
+import {MoveFinger} from './src/systems/MoveFinger';
 
-const App = () => {
-  return <SafeAreaView style={{flex: 1}}></SafeAreaView>;
-};
+export default class App extends PureComponent {
+  constructor() {
+    super();
+  }
 
-export default App;
+  render() {
+    return (
+      <GameEngine
+        style={styles.container}
+        systems={[MoveFinger]}
+        entities={{
+          1: {position: [40, 200], renderer: <Finger />}, //-- Notice that each entity has a unique id (required)
+          2: {position: [100, 200], renderer: <Finger />}, //-- and a renderer property (optional). If no renderer
+          3: {position: [160, 200], renderer: <Finger />}, //-- is supplied with the entity - it won't get displayed.
+          4: {position: [220, 200], renderer: <Finger />},
+          5: {position: [280, 200], renderer: <Finger />},
+        }}>
+        <StatusBar hidden={true} />
+      </GameEngine>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+  },
+});
